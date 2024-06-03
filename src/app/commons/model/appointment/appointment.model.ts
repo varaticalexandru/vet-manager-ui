7327ac3257a9975592882f1c9c1b13f8e4cd4b99
alex <varaticalexandru1@gmail.com>
2024-06-03 20:50:27 +0300

    upd emptyAppointmentsPaginated

--- src/app/commons/model/appointment/appointment.model.ts
+++ src/app/commons/model/appointment/appointment.model.ts
@@ -70,3 +70,24 @@ export interface PaginatedResponse {
   };
   empty: boolean;
 }
+
+export const emptyAppointmentsPaginated: PaginatedResponse = {
+    totalElements: 0,
+    totalPages: 0,
+    size: 5,
+    content: [] as Appointment[],
+    number: 0,
+    sort: { empty: true, sorted: false, unsorted: true },
+    first: true,
+    last: false,
+    numberOfElements: 0,
+    pageable: {
+      pageNumber: 0,
+      pageSize: 5,
+      sort: { empty: true, sorted: false, unsorted: true },
+      offset: 0,
+      paged: true,
+      unpaged: false
+    },
+    empty: true
+}
