d49c27f3b1eb9a1a07f7e4dd0f8758856a2b7ada
alex <varaticalexandru1@gmail.com>
2024-06-06 12:25:23 +0300

    upd appointment routes: add auth-guard [canActivate] to the appointment-list component

--- src/app/components/appointments/api/routes.ts
+++ src/app/components/appointments/api/routes.ts
@@ -1,10 +1,16 @@
-import { Routes } from "@angular/router";
-import { AppointmentListComponent } from "../appointment-list/appointment-list.component";
+import { Routes } from '@angular/router';
+import { AppointmentListComponent } from '../appointment-list/appointment-list.component';
+import { authGuard } from '../../../commons/guards/auth/auth.guard';
 
 export const APPOINTMENT_ROUTES: Routes = [
+  {
+    path: '',
+    component: AppointmentListComponent,
+    canActivate: [authGuard]
+  },
 
-    { path: '', component: AppointmentListComponent },
-
-
-    { path: '**', redirectTo: '' }
-]
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
