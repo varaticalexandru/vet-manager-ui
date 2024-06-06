3d92a79eba7088f33f4c7f4e9dd717743d455187
alex <varaticalexandru1@gmail.com>
2024-06-06 13:58:28 +0300

    upd auth guard: logs out if the jwt token is expired

--- src/app/commons/guards/auth/auth.guard.ts
+++ src/app/commons/guards/auth/auth.guard.ts
@@ -2,19 +2,23 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth-service/auth.service';
 import { map, take } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { FailedAuthComponent } from '../../../components/auth/login/failed-auth/failed-auth.component';
+import { LogOutComponent } from '../../../components/auth/login/log-out/log-out.component';
 
 export const authGuard: CanActivateFn = (route, state) => {
   
   const authService = inject(AuthService);
   const router = inject(Router);
+  const dialog = inject(MatDialog);
 
   return authService.isLoggedIn.pipe(
     take(1),
     map((loggedIn: boolean) => {
-      console.log('logged in', loggedIn);
       
       if (!loggedIn) {
         router.navigate(['/login']);
+        dialog.open(LogOutComponent);
         return false;
       }
       return true;
